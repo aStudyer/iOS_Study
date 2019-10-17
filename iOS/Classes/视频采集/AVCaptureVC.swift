a9bf46978f71ab1120cc7f234777b0d75b20deaa
aStudyer <astudyer@163.com>
2019-10-17 23:02:09 +0800

    视频采集和写入

--- iOS/Classes/视频采集/AVCaptureVC.swift
+++ iOS/Classes/视频采集/AVCaptureVC.swift
@@ -11,12 +11,28 @@ import AVFoundation
 class AVCaptureVC: BaseViewController {
     // MARK: - 懒加载
     private lazy var session: AVCaptureSession = AVCaptureSession()
+    /// 视频输入
+    private lazy var videoDeviceInput: AVCaptureDeviceInput? = {
+        guard let device = (AVCaptureDevice.devices().filter{ $0.position == .front }.first) else {
+            return nil
+        }
+        guard let input = try? AVCaptureDeviceInput(device: device) else {
+            return nil
+        }
+        return input
+    }()
     /// 视频输出
     private lazy var videoDataOutput: AVCaptureVideoDataOutput = {
         let videoDataOutput = AVCaptureVideoDataOutput()
         let queue = DispatchQueue.global()
         videoDataOutput.setSampleBufferDelegate(self, queue: queue)
         return videoDataOutput
+    }()
+    private lazy var fileOutput: AVCaptureMovieFileOutput = {
+        var fileOutput = AVCaptureMovieFileOutput()
+        let connection = fileOutput.connection(with: .video)
+        connection?.automaticallyAdjustsVideoMirroring = true
+        return fileOutput
     }()
     // MARK: - 系统方法
     override func viewDidLoad() {
@@ -33,22 +49,43 @@ class AVCaptureVC: BaseViewController {
 extension AVCaptureVC {
     @IBAction func startCapture(_ sender: UIButton) {
         session.startRunning()
+        // 录制视频，并且写入文件
+        setupMovieFileOutput()
     }
     @IBAction func stopCapture(_ sender: UIButton) {
+        // 停止写入文件
+        fileOutput.stopRecording()
         session.stopRunning()
     }
-}
-// MARK: - 初始化方法
-extension AVCaptureVC {
-    private func initVideoInputOutput() {
-        // 1.初始化视频的输入
-        guard let device = (AVCaptureDevice.devices().filter{ $0.position == .front }.first) else {
+    @IBAction func rotateCamera(_ sender: UIButton) {
+        // 1.取出之前镜头的方向
+        guard let currentVideoInput = videoDeviceInput else {
+            return
+        }
+        let position: AVCaptureDevice.Position = currentVideoInput.device.position == .front ? .back : .front
+        guard let device = (AVCaptureDevice.devices().filter{ $0.position == position }.first) else {
             return
         }
         guard let input = try? AVCaptureDeviceInput(device: device) else {
             return
         }
-        // 2.添加输入输出
+        // 2.移除之前的input，添加新的input
+        session.beginConfiguration()
+        session.removeInput(currentVideoInput)
+        if session.canAddInput(input) {
+            session.addInput(input)
+        }
+        session.commitConfiguration()
+        // 3.保存最新的input
+        videoDeviceInput = input
+    }
+}
+// MARK: - 初始化方法
+extension AVCaptureVC {
+    private func initVideoInputOutput() {
+        guard let input = videoDeviceInput else {
+            return
+        }
         addCaptureToSession(input: input, output: videoDataOutput)
     }
     private func initAudioInputOutput() {
@@ -72,8 +109,8 @@ extension AVCaptureVC {
         // 2.设置frmae
         previewLayer.frame = UIScreen.main.bounds
         // 3.添加预览图层
-        view.layer.addSublayer(previewLayer)
-    }
+        view.layer.insertSublayer(previewLayer, at: 0)
+     }
 }
 // MARK: - 代理方法
 extension AVCaptureVC : AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureAudioDataOutputSampleBufferDelegate {
@@ -83,6 +120,15 @@ extension AVCaptureVC : AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureA
         }else{
             print("采集到音频数据")
         }
+    }
+}
+// MARK: - AVCaptureFileOutputRecordingDelegate
+extension AVCaptureVC: AVCaptureFileOutputRecordingDelegate {
+    func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+        print("开始写入文件")
+    }
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        print("完成写入文件")
     }
 }
 // MARK: - 辅助方法
@@ -96,5 +142,12 @@ extension AVCaptureVC {
             session.addOutput(output)
         }
         session.commitConfiguration()
+    }
+    private func setupMovieFileOutput() {
+        if session.canAddOutput(fileOutput) {
+            session.addOutput(fileOutput)
+        }
+        let filePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first! + "/test.mp4"
+        fileOutput.startRecording(to: URL(fileURLWithPath: filePath), recordingDelegate: self)
     }
 }
